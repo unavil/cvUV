a18faa6962ccec7fb6a31a214059445b58f7da11
Una Vilcane <una.vilcane12@gmail.com>
2022-02-19 21:50:32 +0200

    Initial commit

---
+++ src/main/java/com/cvApplicationUV/cvUV/CvUvApplication.java
@@ -0,0 +1,13 @@
+package com.cvApplicationUV.cvUV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvUvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvUvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvApplicationUV/cvUV/CvUvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvApplicationUV.cvUV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvUvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
